957fd541505e6400492106eca742c12845528a0f
Vincent Leclerc <vleclerc@inow.fr>
2024-05-13 12:41:13 +0200

    router & 404

--- FormationBlazorApp/FormationBlazorApp/Program.cs
+++ FormationBlazorApp/FormationBlazorApp/Program.cs
@@ -9,17 +9,20 @@ builder.Services.AddRazorComponents()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+/*if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+}*/
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+//v8
+app.UseStatusCodePagesWithRedirects("/404");
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
