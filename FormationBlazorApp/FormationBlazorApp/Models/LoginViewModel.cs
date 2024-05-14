19f845410e285df85fc03010de09ebe78fd9cc58
Vincent Leclerc <vleclerc@inow.fr>
2024-05-14 15:20:32 +0200

    store

--- FormationBlazorApp/FormationBlazorApp/Models/LoginViewModel.cs
+++ FormationBlazorApp/FormationBlazorApp/Models/LoginViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FormationBlazorApp.Validators;
+using System.ComponentModel.DataAnnotations;
 
 namespace FormationBlazorApp.Models
 {
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Login obligatoire")]
-        //[Email(ErrorMessage = "Format du mail invalide")]
+        [Email(ErrorMessage = "Format du mail incorrect")]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Mot de passe obligatoire")]
--- FormationBlazorApp/FormationBlazorApp/Program.cs
+++ FormationBlazorApp/FormationBlazorApp/Program.cs
@@ -2,6 +2,7 @@ using FormationBlazorApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ServicesLibrary;
+using Blazored.SessionStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
 
 builder.Services.AddApiServices(builder.Configuration["ApiUrl"]);
+
+builder.Services.AddBlazoredSessionStorageAsSingleton();
 
 await builder.Build().RunAsync();
