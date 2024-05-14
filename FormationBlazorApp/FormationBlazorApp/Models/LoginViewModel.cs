2b31c3eaed8c2525d4f7b52b5f00db5a489f55fa
Vincent Leclerc <vleclerc@inow.fr>
2024-05-14 12:12:46 +0200

    EditContext

--- FormationBlazorApp/FormationBlazorApp/Models/LoginViewModel.cs
+++ FormationBlazorApp/FormationBlazorApp/Models/LoginViewModel.cs
@@ -4,11 +4,11 @@ namespace FormationBlazorApp.Models
 {
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Login obligatoire")]
         //[Email(ErrorMessage = "Format du mail invalide")]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Mot de passe obligatoire")]
         public string? Password { get; set; }
     }
 }
